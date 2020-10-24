c9a899fd0afe486f7ef8a890f973bde8654c5e28
eunbi go <geunbi38@gmail.com>
2020-10-24 17:33:14 +0900

    장비창 하는중

---
+++ SR-Team/Client/Headers/Equip.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#ifndef __EQUIP_H__
+#define __EQUIP_H__
+
+#include "UIObject.h"
+USING(Engine)
+BEGIN(Client)
+
+class CEquip final : public CUIObject
+{
+public:
+	explicit CEquip(LPDIRECT3DDEVICE9 _pDevice, LPD3DXSPRITE _pSprite, LPD3DXFONT _pFont);
+	explicit CEquip(const CEquip& other);
+	virtual ~CEquip() = default;
+
+public:
+	// CUIObject을(를) 통해 상속됨
+	virtual HRESULT Setup_GameObject_Prototype() override;
+	virtual HRESULT Setup_GameObject(void * _pArg) override;
+	virtual _int Update_GameObject(_float _fDeltaTime) override;
+	virtual _int LateUpdate_GameObject(_float _fDeltaTime) override;
+	virtual HRESULT Render_UI() override;
+
+private:
+	HRESULT Add_Component();
+
+public:
+	static CGameObject* Create(LPDIRECT3DDEVICE9 _pDevice, LPD3DXSPRITE _pSprite, LPD3DXFONT _pFont);
+	virtual CGameObject * Clone_GameObject(void * _pArg) override;
+	virtual void Free() override;
+
+private:
+	_bool			m_bRender = true;
+	
+	// 장비창 윈도우
+	CTransform*		m_pTransformCom[EQUIP_END];
+	CTexture*		m_pTextureCom[EQUIP_END];
+	RECT			m_tEquipWndCollRt[EQUIP_END];
+};
+
+
+END
+#endif
